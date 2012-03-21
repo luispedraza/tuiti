dcd3b62688edf976b9badbf1397b78bd8f52b2b0
Luis Pedraza <luispedraza@gmail.com>
2012-03-21 20:49:46 +0100

    modificadas las páginas estáticas

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,21 +4,41 @@ describe "StaticPages" do
 	describe "Home page" do
 		it "should have the content 'Sample App'" do
 			visit '/static_pages/home'
-			page.should have_content('Sample App')
+			page.should have_selector('h1', 
+				:text => 'Sample App')
+		end
+		it "should have the right title" do
+			visit '/static_pages/home' do
+				page.shoud have_selector('title', 
+					:text => "Tuiti | Home")
+			end
 		end
 	end
 
 	describe "Help page" do
 		it "should have the content 'Help'" do
 			visit '/static_pages/help'
-			page.should have_content('Help')
+			page.should have_selector('h1',
+				:text => 'Help')
+		end
+		it "should have the right title" do
+			visit '/static_pages/help' do
+				page.shoud have_selector('title', 
+					:text => "Tuiti | Help")
+			end
 		end
 	end
 
 	describe "About page" do
-		it "hould have the content' About us'" do
+		it "hould have the content 'About us'" do
 			visit '/static_pages/about'
 			page.should have_content('About us')
+		end
+		it "should have the right title" do
+			visit '/static_pages/about' do
+				page.shoud have_selector('title', 
+					:text => "Tuiti | About")
+			end
 		end
 	end
 end
