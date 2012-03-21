4950d370244e28bfa48f061956924e0cf9086c48
Luis Pedraza <luispedraza@gmail.com>
2012-03-21 19:44:13 +0100

    Creación de la aplicación

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tuiti::Application.config.secret_token = '68e9aa42872403cbd0bd43adae97141f774a006009cb108e01ad555dbc692ae839cbb0ab01be995f23f55627b9e701d2bc94a0b650019bd4dc9eae886e73d9d3'
