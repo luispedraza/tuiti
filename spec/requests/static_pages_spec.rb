8ad917fee9d96f9c5e85eee49e7395240853b975
Luis Pedraza <luispedraza@gmail.com>
2012-03-22 18:50:37 +0100

    nada que decir

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe "StaticPages" do
 	describe "Home page" do
-		it "should have the content 'Sample App'" do
+		it "should have the h1 'Sample App'" do
 			visit '/static_pages/home'
 			page.should have_selector('h1', 
 				:text => 'Sample App')
 		end
-		it "should have the right title" do
+		it "should have the title Home" do
 			visit '/static_pages/home' do
 				page.shoud have_selector('title', 
 					:text => "Tuiti | Home")
@@ -16,12 +16,12 @@ describe "StaticPages" do
 	end
 
 	describe "Help page" do
-		it "should have the content 'Help'" do
+		it "should have the h1 'Help'" do
 			visit '/static_pages/help'
 			page.should have_selector('h1',
 				:text => 'Help')
 		end
-		it "should have the right title" do
+		it "should have the title Help" do
 			visit '/static_pages/help' do
 				page.shoud have_selector('title', 
 					:text => "Tuiti | Help")
@@ -30,14 +30,29 @@ describe "StaticPages" do
 	end
 
 	describe "About page" do
-		it "hould have the content 'About us'" do
+		it "hould have the h1 'About us'" do
 			visit '/static_pages/about'
-			page.should have_content('About us')
+			page.should have_selector('h1',
+				:text => 'About us')
 		end
-		it "should have the right title" do
+		it "should have the title About" do
 			visit '/static_pages/about' do
 				page.shoud have_selector('title', 
 					:text => "Tuiti | About")
+			end
+		end
+	end
+
+	describe "Contact page" do
+		it "hould have the h1 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1',
+				:text => 'Contact')
+		end
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact' do
+				page.shoud have_selector('title', 
+					:text => "Tuiti | Contact")
 			end
 		end
 	end
