ec58176c9c010bcc479f107e5fe5f43f85480704
Luis Pedraza <luispedraza@gmail.com>
2012-04-17 10:24:34 +0200

    modificaciones de textos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -17,4 +19,30 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Perfil actualizado correctamente"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  
+  private
+  def signed_in_user
+    redirect_to signin_path, notice: "Por favor, inicia sesion" unless signed_in?
+  end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+  
 end
